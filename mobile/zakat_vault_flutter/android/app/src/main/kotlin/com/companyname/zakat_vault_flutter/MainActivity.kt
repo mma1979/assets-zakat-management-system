f695fc9ba2c72760a0d7a54459ef1d3313c4f4a5
Mohammed Abdelhay <m.abdelhay.c@nhci.sa>
2026-08-31 15:00:22 +0300

    remove flutter app

--- mobile/zakat_vault_flutter/android/app/src/main/kotlin/com/companyname/zakat_vault_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.companyname.zakat_vault_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile/zakat_vault_flutter/lib/core/constants.dart
+++
@@ -1,9 +0,0 @@
-class AppConstants {
-  static const String baseUrl = 'http://10.0.2.2:5000/api';
-
-
-  // Storage keys
-  static const String tokenKey = 'auth_token';
-  static const String refreshTokenKey = 'auth_refresh_token';
-  static const String userKey = 'auth_user';
-}
